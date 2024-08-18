0d0256dd1f1833b04cc1374b6ffe8bbdb46d421e
Leinad Roczak <daniel.kac94@gmail.com>
2024-08-18 20:07:36 +0200

    Update build script, gitignore, del unnecessary

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,22 +1,24 @@
-
 #!/bin/bash
 
 # Define source files
 SOURCE_FILES="main.c AnalogFilters.c DigitalFilters.c BillinearTransform.c"
 
-# Define output executable name
-OUTPUT_EXECUTABLE="main"
+# Define output folder and executable name
+OUTPUT_DIR="output"
+OUTPUT_EXECUTABLE="$OUTPUT_DIR/main"
 
-# Compile the source files
+# Create the output directory (if it doesn't exist already)
+mkdir -p $OUTPUT_DIR
+
+# Compile the source files and place them in the output directory
 gcc -fdiagnostics-color=always -g $SOURCE_FILES -o $OUTPUT_EXECUTABLE
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable: $OUTPUT_EXECUTABLE"
     
-    # Run the executable
+    # Run the executable from the output directory
     ./$OUTPUT_EXECUTABLE
-
 else
     echo "Compilation failed."
 fi
